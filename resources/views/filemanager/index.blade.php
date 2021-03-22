4b893a61a1b13a55d4b4be5aa4aa2c237b785588
Louis <louis@b-teka.com>
2021-03-22 09:35:55 +0700

    Upgrade admin password

--- resources/views/filemanager/index.blade.php
+++ resources/views/filemanager/index.blade.php
@@ -28,8 +28,7 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'admin' => '$2y$10$XTElZ5qUk9e5oM7mX2ZvXO15D0bWjprgH7R2rXXSHeMHsT1x8x5kC'
 );
 
 //set application theme
